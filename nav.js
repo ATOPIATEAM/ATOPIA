f1820e6a06a09751ad947f8d5ef71b154487514b
staticoly <113824009+staticoly@users.noreply.github.com>
2023-10-20 12:38:01 +0100

    Updates

--- nav.js
+++ nav.js
@@ -6,11 +6,14 @@ navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute("data-visible");
 
     if(visibility === "false"){
-        primaryNav.setAttribute('data-visible', true);    
+        primaryNav.setAttribute("data-visible", true);    
+        navToggle.setAttribute("aria-expanded", true);
     }
     else if(visibility === "true"){
         primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false)
     }
 console.log('true')
 
 })
+
