be23bcf2c0907cbde3dca59f438a60f1e22377b8
ATOPIA <113824009+staticoly@users.noreply.github.com>
2023-12-02 09:22:07 -0800

    updates

--- nav.js
+++ nav.js
@@ -1,19 +1,2 @@
-const primaryNav = document.querySelector('ul#primary-navigation')
-const navToggle = document.querySelector('.mobile-nav-toggle')
-
-navToggle.addEventListener('click', () => {
-
-    const visibility = primaryNav.getAttribute("data-visible");
-
-    if(visibility === "false"){
-        primaryNav.setAttribute("data-visible", true);    
-        navToggle.setAttribute("aria-expanded", true);
-    }
-    else if(visibility === "true"){
-        primaryNav.setAttribute("data-visible", false);
-        navToggle.setAttribute("aria-expanded", false)
-    }
-console.log('true')
-
-})
+const budgetSeList
 
