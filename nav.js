cadfbe591c459d1992cd1fc529b2029a8abc2dff
ATOPIA <113824009+staticoly@users.noreply.github.com>
2023-12-03 07:08:20 -0800

    form submission integration

--- nav.js
+++ nav.js
@@ -1,3 +1,5 @@
+
+//responsive nav bar toggle
 const primaryNav = document.querySelector('ul#primary-navigation')
 const navToggle = document.querySelector('.mobile-nav-toggle')
 navToggle.addEventListener('click', () => {
@@ -15,14 +17,30 @@ console.log('true')
 
 })
 
-
+//budget click color change
 function changeColors(clickedElement) {
-  // Remove 'clicked' class from all budget elements
   var budgetElements = document.querySelectorAll('.budget');
   budgetElements.forEach(function (element) {
-    element.classList.remove('clicked');
+      element.classList.remove('clicked');
   });
 
-  // Toggle 'clicked' class for the clicked element
   clickedElement.classList.toggle('clicked');
+
+  // Set the value of the hidden input field based on the selected budget
+  var selectedBudget = clickedElement.querySelector('.budget-range').textContent;
+  document.querySelector('#contactForm input[name="selectedBudget"]').value = selectedBudget;
 }
+
+
+//timeline default color 
+document.addEventListener('DOMContentLoaded', function () {
+  var timelineSelect = document.getElementById('Timeline-3');
+
+  timelineSelect.addEventListener('change', function () {
+      if (timelineSelect.value !== "") {
+          timelineSelect.classList.add('timeline-selected');
+      } else {
+          timelineSelect.classList.remove('timeline-selected');
+      }
+  });
+});
