f19d527b51afcf825e2fcbece5a3f5ece2632208
ATOPIA <113824009+staticoly@users.noreply.github.com>
2023-12-02 23:10:22 -0800

    button update

--- nav.js
+++ nav.js
@@ -1,2 +1,10 @@
-const budgetSeList
+function changeColors(clickedElement) {
+  // Remove 'clicked' class from all budget elements
+  var budgetElements = document.querySelectorAll('.budget');
+  budgetElements.forEach(function (element) {
+    element.classList.remove('clicked');
+  });
 
+  // Toggle 'clicked' class for the clicked element
+  clickedElement.classList.toggle('clicked');
+}
